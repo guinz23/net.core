795c0e3281c0bd5a6213767d3dd2d177a63b49c9
guinz23 <juangabriellopez30@gmail.com>
2018-02-05 18:46:54 -0600

    tarea

---
+++ mvc1/mvc1/Pages/carreras.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mvc1.Pages
+{
+    public class carrerasModel : PageModel
+    {
+        public string[] Carreras { get; set; }
+        public string resultado;
+       
+        public void OnGet()
+        {
+            Carreras= new string[] { "CATERING SERVICE", "PERSONAL CHEF", "Waiters", "LIMOSINA SERVICE" };
+
+
+        }
+        public  string timertime() {
+
+            resultado = DateTime.Now.ToString("dd/MM/yyyy");
+
+            return resultado;
+            }
+        }
+    }
